fea020eb2bef519c61dcadf8c0a50564f7d58e4c
Adam Bedford <abedford@mavenlink.com>
2015-12-16 20:37:50 -0800

    Initial commit of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Marketing::Application.config.secret_key_base = '671c20a185ac6c87697ea2da2987d887e09e368a92124c043edefa013a6330c848f6ebd1270d210969b9b58a8249dfd69f3639a531d8232661a4739a829d9a88'
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Marketing::Application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root to: 'high_voltage/pages#show', id: 'home'
+
+end
